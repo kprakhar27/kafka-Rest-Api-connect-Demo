7b010b1a4e1ae6153c1c8d379873d0db59e69928
adderbyte <adderbyte@users.noreply.github.com>
2021-09-18 22:00:16 +0200

    updated rest APi sh

--- setup-kafka-restApi.sh
+++ setup-kafka-restApi.sh
@@ -1,36 +1,32 @@
-#!/bin/sh
-
 set -e
 set -x
 
 ################
 
-echo "*** building kafka-rest api  kafka..."
+# echo "*** building kafka-rest api up kafka..."
 
-#VERSION=3.4.5
-
-######################################3
-### uncomment out this once
+# # #VERSION=3.4.5
 mkdir -p tmp
 
 mkdir -p docker/rest-connector/tmp
 
-# git clone https://github.com/llofberg/kafka-connect-rest.git tmp/rest-connector
+git clone https://github.com/llofberg/kafka-connect-rest.git tmp/rest-connector
+unzip -o tmp/kafka-connect-rest.zip -d tmp/
 
+# # #cp -r tmp/kafka-connect-rest \
 cd tmp/rest-connector
-# mvn clean install && \
-# cd examples/spring/gs-rest-service && \
-# mvn clean install 
+mvn clean install && \
+cd examples/spring/gs-rest-service && \
+mvn clean install 
+&& \
+# # # cd .. && \
+# # # cp ../../kafka-connect-rest-plugin/target/kafka-connect-rest-plugin-*-shaded.jar jars/ && \
+# # # cp ../../kafka-connect-transform-from-json/kafka-connect-transform-from-json-plugin/target/kafka-connect-transform-from-json-plugin-*-shaded.jar jars/ && \
+# # # cp ../../kafka-connect-transform-add-headers/target/kafka-connect-transform-add-headers-*-shaded.jar jars/ && \
+# # # cp ../../kafka-connect-transform-velocity-eval/target/kafka-connect-transform-velocity-eval-*-shaded.jar jars/    
 
 
-###########################################
-## clean this
-# && \
-# cd .. && \
-# cp ../../kafka-connect-rest-plugin/target/kafka-connect-rest-plugin-*-shaded.jar jars/ && \
-# cp ../../kafka-connect-transform-from-json/kafka-connect-transform-from-json-plugin/target/kafka-connect-transform-from-json-plugin-*-shaded.jar jars/ && \
-# cp ../../kafka-connect-transform-add-headers/target/kafka-connect-transform-add-headers-*-shaded.jar jars/ && \
-# cp ../../kafka-connect-transform-velocity-eval/target/kafka-connect-transform-velocity-eval-*-shaded.jar jars/    
+###########
 
 ################
 
@@ -52,8 +48,14 @@ cp kafka-connect-transform-from-json/kafka-connect-transform-from-json-plugin/ta
 
 ################
 
+################
+
+# echo "*** Kafkaka connector is jdbc ..."
+
+#mvn package -f custom-extensions/pom.xml
+#cp custom-extensions/target/custom-extensions-1.0-SNAPSHOT.jar \
+#       docker/rest-connector/tmp/custom-extensions.jar
+
+################
 
 
-# cd docker
-# docker-compose build
-# docker-compose up -d
